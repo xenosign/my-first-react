3023b0dd2e310465c97e2cec55ad698a69604d57
kdtTetz <xenosign@naver.com>
2022-12-10 18:13:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,27 @@ import ImgComponent from "./components/ImgComponent";
 import MainHeader from "./components/MainHeader";
 
 function App() {
+  const pororoObjArr = [
+    {
+      name: "뽀로로",
+      age: "5",
+      nickName: "사고뭉치",
+    },
+    {
+      name: "루피",
+      age: "4",
+      nickName: "공주님",
+    },
+    {
+      name: "크롱이",
+      age: "5",
+      nickName: "장난꾸러기",
+    },
+  ];
   return (
     <div className="App">
-      <Comparing />
+      <ChangeObj objArr={pororoObjArr} />
     </div>
   );
 }
-
 export default App;
